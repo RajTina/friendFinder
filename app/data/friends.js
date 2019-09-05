3ffbb8049adde9adc10855977dfcfae2f6759e4a
Tina Azad <mktazad@gmail.com>
2019-09-04 22:30:04 -0400

    finished

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Sharukh Khan",
+		photo: "http://www.afghanistantimes.af/wp-content/uploads/2014/11/8824025_1411384031.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Salman Khan",
+		photo: "https://www.livelaw.in/cms/wp-content/uploads/2014/09/Salman-Khan.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Ferdous Ahmed",
+		photo: "http://www.theindependentbd.com/assets/news_images/Ferdous.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Kajol Ajay",
+		photo: "https://starsunfolded.com/wp-content/uploads/2014/06/Kajol.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Moushumi Sani",
+		photo: "https://alchetron.com/cdn/moushumi-54b7afa1-dadc-4fdf-830e-6a260c80450-resize-750.jpeg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
